958801b16e6447348c24f89ead0aa615a82bccad
Taylor Peat <taylorpeat@hotmail.com>
2016-08-30 12:23:08 -0400

    Bob exercise

--- bob/bob.js
+++ bob/bob.js
@@ -1,14 +1,16 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  if ( input.toUpperCase() === input &&
+       input.toLowerCase() !== input ) {
+    return "Whoa, chill out!";
+  } else if (input.endsWith("?")) {
+    return "Sure.";
+  } else if (!/\S/.test(input)) {
+    return "Fine. Be that way!";
+  } else {
+    return "Whatever.";
+  }
 };
 
 module.exports = Bob;
